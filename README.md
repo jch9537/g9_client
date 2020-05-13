6782c350bd857fae9db5e475bff53b3fc2a5afa6
jch9537 <jch9537@gmail.com>
2020-05-14 00:14:15 +0900

    move ID_validation_function to server

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # g9_client
 
-테스트 과제 클라이언트
+## 테스트 과제 클라이언트
+
+주민번호 유효성 확인 후 네이버 실시간 검색어 추출
+사용 언어 : javascript
+
+1. input 창 작성 글자수 제한
+2. input 창 숫자 이외의 문자입력 제한
+3. 실시간 검색어 렌더
+
+- Server URI : http://localhost:8000
+- Endpoint : /naver/realtime
+- Resource : IdNumber
+- Method : POST
+- Success Response : { data: data, message: "가져오기 완료" }
+- Error Response : :유효하지 않은 주민번호 일 때 {error: { status: 400, message: "유효한 주민번호가 아닙니다." }}, 서버오류 { error: { status: 500, message: "서버오류" } }
--- index.js
+++ index.js
@@ -1,46 +1,39 @@
 const server = "http://localhost:8000";
-
 //fetch함수
 function api(url, method, data) {
-  //   console.log(url, method, data);
-  //   let body = { id: data };
   if (method === "GET") {
-    console.log("겟");
     return fetch(`${server}${url}`, {
       method,
       mode: "cors",
       headers: {
         "Content-Type": "application/json",
       },
-      //   credentials: "include"
     })
       .then((res) => res.json())
       .then((res) => {
         console.log("응답겟 ------------------------", res);
-        renderSearchWord(res);
         if (res.error) {
+          console.log("에러겟 ------------------------", res.error);
           return Promise.reject(res.error);
         } else {
           return res;
         }
       });
   } else {
-    // console.log("111111", `${server}${url}`);
-    // console.log(JSON.stringify(data));
+    const body = { id: data };
     return fetch(`${server}${url}`, {
       method,
       mode: "cors",
       headers: {
         "Content-Type": "application/json",
       },
-      // credentials: "include",
       body: JSON.stringify(body),
     })
       .then((res) => res.json())
       .then((res) => {
-        console.log("응답포스트 ------------------------", res);
-        renderSearchWord(res);
+        // console.log("응답포스트 ------------------------", res);
         if (res.error) {
+          //   console.log("에러포스트 ------------------------", res.error);
           return Promise.reject(res.error);
         } else {
           return res;
@@ -48,76 +41,43 @@ function api(url, method, data) {
       });
   }
 }
+//실시간 검색어 렌더
 function renderSearchWord(array) {
+  const searchTitle = document.querySelector(".searchWord_title");
+  searchTitle.style = "visibility: visible;";
   for (let i = 0; i < array.length; i++) {
     const listElement = document.getElementById("searchWord_list");
-
     const searchItem = document.createElement("li");
     searchItem.className = "search_item";
     for (let key in array[i]) {
-      searchItem.innerHTML = `<span className='search_itemNum'>${key} </span><span className='serch_itemWord'> ${array[i][key]}</span>`;
+      searchItem.innerHTML = `<span class='search_itemNum'>${key} </span><span class='serch_itemWord'> ${array[i][key]}</span>`;
     }
-    console.log(searchItem);
     listElement.appendChild(searchItem);
   }
 }
 //input 자리수 제한
 function checkNumberLength(e) {
-  console.log(e.value, e.maxLength);
   if (e.value.length > e.maxLength) {
     e.value = e.value.slice(0, e.maxLength);
   }
 }
-
-//숫자인지 확인하는 함수
-function checkStringNumber(num) {
-  var reg = /\d{13}/;
-  return reg.test(Number(num));
-}
-
-//주민번호 유효성확인
-function checkIdNumber(serialNumber) {
-  if (serialNumber.length !== 13) {
-    return false;
-  }
-  if (checkStringNumber(serialNumber)) {
-    // console.log("시리얼넘버", typeof serialNumber);
-    let checkArray = [2, 3, 4, 5, 6, 7, 8, 9, 2, 3, 4, 5];
-    let sum = 0;
-    let numbers = serialNumber.split("");
-    for (let i = 0; i < numbers.length - 1; i++) {
-      sum += numbers[i] * checkArray[i];
-    }
-    let extraNumber = sum % 11;
-    let lastNumber = (11 - extraNumber) % 10;
-    // console.log("합계", sum, "익스트라", extraNumber, "마지막번호", lastNumber);
-    // console.log(numbers[numbers.length - 1]);
-    if (Number(numbers[numbers.length - 1]) === lastNumber) {
-      return true;
-    } else {
-      return false;
-    }
-  } else {
-    return false;
-  }
-}
-
 //이벤트 함수
 document.getElementById("submit").addEventListener("click", function () {
   let idNumber1 = document.getElementById("idNumber1").value;
   let idNumber2 = document.getElementById("idNumber2").value;
-  console.log("----------------", idNumber1, idNumber2);
   let idNumber = idNumber1 + idNumber2;
-  console.log(idNumber, typeof idNumber);
-  if (checkIdNumber(idNumber)) {
-    api("/naver/realtime", "GET");
-    document.getElementById("idNumber1").value = "";
-    document.getElementById("idNumber2").value = "";
-  } else {
-    alert("유효한 주민번호가 아닙니다.");
-    document.getElementById("idNumber1").value = "";
-    document.getElementById("idNumber2").value = "";
-  }
+  //or  api(`/naver/realtime/${idNumber}`, "GET")
+  api("/naver/realtime", "POST", idNumber)
+    .then((res) => {
+      console.log(res.message);
+      renderSearchWord(res.data);
+    })
+    .catch((error) => {
+      console.log(`${error.message} : 에러코드 ${error.status}`);
+      alert(error.message);
+    });
+  document.getElementById("idNumber1").value = "";
+  document.getElementById("idNumber2").value = "";
 });
 
-//할 것 : input 리스트안나오게하기, form으로 처리해보기(label도 확인), 모듈로 나누기
+//할 것 : form으로 처리해보기(label도 확인)
